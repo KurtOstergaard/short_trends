4ec1b7468cacf19e78b0d5c061c14db5b9833168
Kurt Ostergaard <yodude@gmail.com>
2023-08-23 00:00:03 -0400

    Got Heikin Ashi working; first draft of entire scenario evaluation all the way throu summary trade table.

--- main.R
+++ main.R
@@ -9,7 +9,7 @@ library(TTR)
 library(plotly)
 library(profvis)
 library(microbenchmark)
-
+library(rlang)
 
 ################ C code for EMA calculation with no leading NA
 sourceCpp(
@@ -30,8 +30,7 @@ sourceCpp(
      }
     ")
 
-#################### Heikin Ashi
-# https://stackoverflow.com/questions/53941102/quantmod-heikin-ashi-plotting-unavailable
+#################### Heikin Ashi - 'average pace' also translates as 'average foot'
 Rcpp::cppFunction('NumericMatrix RawHeikinAshi(NumericMatrix x, CharacterVector n) {
 // assumes OHLC matrix input
 int nrow = x.nrow(), ncol = 4, Op=0, Hi=1, Lo=2, Cl=3;
@@ -52,71 +51,135 @@ HAOHLC <- function(x) {
   return(reclass(r, x))
 }
 
-# p <- getSymbols("SPY")
-# microbenchmark(HAOHLC(p), times = 100L, unit = "ms")
-# microbenchmark(HAOHLC(p), heikin_ashi(p), times = 100L, unit = "ms")
-
-# Unit: milliseconds
-# expr       min          lq        mean     median         uq        max neval
-# HAOHLC(p)   0.36409   0.4275205   0.5198086   0.502614   0.552392   1.378134   100
-# heikin_ashi(p) 563.33925 582.6144955 609.0082902 591.338550 620.179235 802.885348   100
-
-################## Heikin Ashi 2 
-
-
-heikin_ashi <- function(data) {
-  
-  if(!quantmod::is.OHLC(data)) stop("data must contain OHLC columns")
-  
-  heikin_close <- xts::xts(Matrix::rowMeans(quantmod::OHLC(data)), order.by = index(data))
-  heikin_open  <- quantmod::Op(data)
-  
-  # need a loop: heiki ashi open is dependent on the previous value
-  for(i in 2:nrow(data)) {
-    heikin_open[i] <- (heikin_open[i-1] + heikin_close[i-1]) / 2
-  }
-  
-  heikin_high <- xts::xts(apply(cbind(quantmod::Hi(data), heikin_open, heikin_close), 1, max), order.by = index(data))
-  heikin_low <- xts::xts(apply(cbind(quantmod::Lo(data), heikin_open, heikin_close), 1, min), order.by = index(data))
-  
-  out <- merge(heikin_open, heikin_high, heikin_low, heikin_close)
-  out <- setNames(out, c("Open", "High", "Low", "Close"))
-}
-
-
-
-ADM <- getSymbols("ADM", from = "2018-10-01", auto.assign = FALSE)
-ha_ADM <- heikin_ashi(AMD)
-chartSeries(ha_ADM) # or chart_Series or rtsplot or geom_candlestick
-
-############################# end of Heikin Ashi
-
-
-
-m30 <- read_csv("CME_MINI_NQ1!, 30_ac16d.csv", col_names = TRUE)
-h1 <- read_csv("CME_MINI_NQ1!, 60_f371d.csv", col_names = TRUE)
-h2 <- read_csv("CME_MINI_NQ1!, 120_925b9.csv", col_names = TRUE)
-h3 <- read_csv("CME_MINI_NQ1!, 180_62ca4.csv", col_names = TRUE)
+# Start dates: 1hr starts 1/1/20, 2hr 1/2/17, 3hr & 4hr 8/14/13, 6hr 1/2/04
+# m30 <- read_csv("CME_MINI_NQ1!, 30_ac16d.csv", col_names = TRUE)
+# h1 <- read_csv("CME_MINI_NQ1!, 60_f371d.csv", col_names = TRUE)
+# h2 <- read_csv("CME_MINI_NQ1!, 120_925b9.csv", col_names = TRUE)
+# h3 <- read_csv("CME_MINI_NQ1!, 180_62ca4.csv", col_names = TRUE)
 h4 <- read_csv("CME_MINI_NQ1!, 240_48328.csv", col_names = TRUE)
-h6 <- read_csv("CME_MINI_NQ1!, 360_2a174.csv", col_names = TRUE)
-spec(h6)
+# h6 <- read_csv("CME_MINI_NQ1!, 360_2a174.csv", col_names = TRUE)
+# spec(h6)
 
-h4HA <- HAOHLC(h4)
-h4HA <- h4HA |> rownames_to_column("time") 
-h4HA$time <- as.POSIXct(h4HA$time, tz=Sys.timezone())
+h4HA <- HAOHLC(h4) 
+h4HA <- h4HA |> rownames_to_column("time") |>
+  mutate(real_open = h4$open,
+         real_close = h4$close,
+         spread = lead(real_open) - real_close,
+         time = as.POSIXct(time, tz=Sys.timezone())) |>
+  na.omit() |>
+  as_tibble() 
+  
 
-h4b <- h4 
-h4c <- full_join(h4b, h4HA)
+# max(h4HA$spread) # 171 Wow!
+# min(h4HA$spread) # -287.5 double Wow!
+# holy fuck! Make a histogram of this craziness to estimate buy skid
+skid <- 1
+
+results <- tibble() 
+colnames(results) <- unlist(str_split("j, slow_lag, fast_lag,
+            ICAGR, drawdown, bliss, lake, end_val, trade_test", ", "))
+
+# for (j in seq(3, 5, 1)){   #initiate optimization sequence
+j <- 5
+  df <- h4HA
+  df$lag <- ewmaRcpp(df$ha.close, j)
   
+  df<- df |>                # create trade signal 
+    mutate(cross = ha.close - lag,
+           on = ifelse(cross > 0 & lag(cross) < 0, 1, 0),
+           off = ifelse(cross < 0 & lag(cross) > 0, -1, 0))
+  df <- slice(df, 30:n()) # drop first 30 rows to warm up EMA & Heikin Ashi
   
+  if(df$cross[1] > 0) df$on[1] <- 1  # first row
+  df <- df|>                         # trade signal details 
+    mutate(signal = on + off,
+          buy_date = ifelse(on == 1, as_datetime(lead(time)), 0),
+          buy_price = ifelse(on == 1, real_close + skid, 0),
+          buy_amount = 0,
+          sell_date = ifelse(off == -1, as_datetime(lead(time)), 0),
+          sell_price = ifelse(off == -1, real_close - skid, 0))
+
+  if (df$cross[nrow(df)] > 0) {    # close out trade if long at EOF
+  df$off[nrow(df)] <- -1
+  df$signal[nrow(df)] <- -1
+  df$sell_date[nrow(df)] <- df$time[nrow(df)]
+  df$sell_price[nrow(df)] <- df$real_close[nrow(df)] - skid
+}
+ 
+  start_value <- 1e5 ; df$closed_pnl <- 1e5 ; df$highwater <- 1e5
+  df$open_pnl <- 0 ; df$equity <- 0 ; df$water <- 0
+  df$drawdown <- 0 ; buy_amount <- 0 ; buy_price <- 0
+  # heat <- 0.1 # risk budget; not currently used, ATR_multiplier not used either
   
+  # i=1 pnl trade calculation
+  if(df$signal[1] == 1) {
+    buy_amount = 1
+    df$buy_amount[1] = buy_amount
+    buy_price = df$buy_price[1]
+  }
+  # i>1 pnl trade calculation
+  for (i in seq2(2, nrow(df))) {
+    df$closed_pnl[i] = df$closed_pnl[i-1]
+    if(df$signal[i] == -1) {
+      df$closed_pnl[i] = df$closed_pnl[i] + 
+        (df$sell_price[i] - buy_price) * buy_amount
+      buy_amount = 0
+      df$open_pnl[i] = 0
+    } else if(df$signal[i] == 1) {
+      buy_amount = 1
+      df$buy_amount[i] = buy_amount
+      buy_price = df$buy_price[i]
+    } else {
+      df$open_pnl[i] = (df$real_close[i] - buy_price) * buy_amount
+    }
+    df$equity[i] <- df$open_pnl[i] + df$closed_pnl[i]
+    df$highwater[i] <- pmax(df$equity[i], df$highwater[i-1])
+    df$water[i] <- df$highwater[i] - df$equity[i]
+    df$drawdown[i] <- df$water[i] / df$highwater[i]
+  }
+  # summary trade table extract (or extrusion?)
+  trade_test <- sum(df$signal)
+  if(trade_test == 0) {
+    buys <- df |>
+      select(on, buy_date, buy_price, buy_amount) |>
+      filter(on == 1) |>
+      select(!on)
+    sells <- df |>
+      select(off, sell_date:drawdown) |>
+      filter(off == -1) |>
+      select(!off)
+    trades <- bind_cols(buys, sells)
+    trades$buy_date <- as.Date(as.numeric(trades$buy_date))
+    trades$sell_date <- as.Date(as.numeric(trades$sell_date))
+    trades$trade_pnl <- (trades$sell_price - trades$buy_price) * trades$buy_amount
+  }
+  z <- df$equity[nrow(df)]
+z
+zz <- sum(trades$trade_pnl)
+zz
+# calc win/lose count and avg win/lose $        Start here!
+win_count <- trades |>
+  select(trades$trade_pnl > 0) |>
+  nrow()
+
+
+# } optimization end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 sketch <- tibble() |>
   colnames(unlist(list("m30", "h1", "h2", "h3", "h4", "h6")))
-
-test1 <- filter(h1, time > "2023-08-13" & time < "2023-08-18")
-test6 <- filter(h6, time > "2023-08-13" & time < "2023-08-18")
-
-
 
 evry <- bind_rows(h6, h4, h3, h2, h1, m30, .id="id") 
 
